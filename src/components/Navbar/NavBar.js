8292a74684df0b5d02b517b73c428668f10a572f
Ivan Ahmed Adi <ivo.adi_18@hotmail.com.ar>
2022-06-12 14:38:19 -0300

    homeroute

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,11 +1,11 @@
 import CartWidget from '../CartWidget/CartWidget'
-
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 function NavBar() {
     return (
         <nav className="navv">
-            <h1 className="huno">WaterStore</h1>
+          <Link to='/'><h1 className="huno">WaterStore</h1></Link>
             <div className="divnav">
 
                 <button className="btn">Celulares</button>
