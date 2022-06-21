bec2f169a307d1fda7d02bb6c39e4babcf3b8958
Ivan Ahmed Adi <ivo.adi_18@hotmail.com.ar>
2022-06-20 22:17:46 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route, } from "react-router-dom";
 import './App.css';
 import NavBar from './components/Navbar/NavBar';
 import ItemListContainer from './components/ItemListConteiner/ItemListConteiner';
-import ItemCount from './components/ItemCount/ItemCount';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -24,7 +23,7 @@ function App() {
           <Route path='/item/:id' element={<ItemDetailContainer />} />
         </Routes>
       </section>
-      <ItemCount stock={10} />
+       
     </div>
     
     </>
